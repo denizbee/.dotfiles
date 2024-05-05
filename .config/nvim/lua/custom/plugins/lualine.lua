9dbb4708345bd1cc1dca6ece4294314502eeb533
denizbee <denizba>
2024-05-05 18:17:19 +0300

    Switched to catppuccin theme

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -5,7 +5,6 @@ return {
     config = function()
       require('lualine').setup {
         options = {
-          theme = 'catppuccin',
           disabled_filetypes = {
             'NvimTree',
           },
