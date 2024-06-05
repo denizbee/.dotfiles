9a16502581fe5df0a4c647759b7e22d54d485a21
denizbee <denizba>
2024-06-06 00:06:53 +0300

    Gruv

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,9 @@
 -- Set language to en_US
 vim.cmd 'language en_US'
 
+-- Set theme
+vim.cmd.colorscheme 'retrobox'
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
